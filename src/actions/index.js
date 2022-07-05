b2598adbc9a2c573a5f4640e9d5b3e2cbeea3781
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-07-06 01:20:21 +0200

    refactor code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder"
 
 //because of using async await the code below doesnt return js plain object. so oour acttion creator is not working as expected
-export const fetchPost = async() => {
+export const fetchPost = () => async dispatch => {
 
   //thunk func
-  return async dispatch => {
     //in this innner func we dontt need to return any action, if we are returning a function
     // then if we only want to dispatch an action we will insttead cann the dispatch function 
     // manually with the action we would like to dispatch
@@ -14,7 +13,7 @@ export const fetchPost = async() => {
       type: 'FETCH_POSTS',
       payload: result
     })
-  }
+  
 }
 
 export const selectPost = () => {
